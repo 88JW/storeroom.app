ef8102a0bf423a7d76491088aca0c195414bdb3b
w.jaskula8 <w.jaskula8@gmail.com>
2025-07-23 08:01:29 +0000

    Add icons, stich UI files, and update project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// W przyszłości zaimportujemy tutaj nasze komponenty stron, np.:
+// import LoginPage from './pages/LoginPage';
+// import ListPage from './pages/ListPage';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        {/* Trasa dla strony logowania */}
+        {/* element={<LoginPage />} */}
+        <Route path="/logowanie" element={<div>Strona Logowania</div>} />
+
+        {/* Trasa dla strony listy */}
+        {/* element={<ListPage />} */}
+        <Route path="/lista" element={<div>Strona Listy</div>} />
+
+        {/* Domyślna trasa lub przekierowanie */}
+        {/* Możemy tutaj dodać np. przekierowanie na stronę logowania */}
+        <Route path="*" element={<div>Strona nie znaleziona lub domyślna</div>} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: 'auto',
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+    },
+    manifest: {
+      name: 'Fridge Manager App',
+      short_name: 'FridgeApp',
+      description: 'Aplikacja do zarządzania zawartością lodówki i zamrażarki',
+      theme_color: '#1993e5',
+      icons: [
+        {
+          src: '/icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })],
 })
